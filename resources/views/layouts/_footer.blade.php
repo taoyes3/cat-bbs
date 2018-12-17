4a8ff792e249bd45f4451ea1c932d61bbdb99df6
921t <Timi921t@gmail.com>
2018-12-17 04:51:08 +0000

    添加注册验证码

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="#">921t</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            <a href="#">921t</a> <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="pull-right"><a href="mailto:1844325217@qq.com">联系我们</a></p>
