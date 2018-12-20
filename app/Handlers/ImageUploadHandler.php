3931b823120e61b6c1297ebc6ab79f3c63d156df
921t <Timi921t@gmail.com>
2018-12-20 04:23:04 +0000

    头像上传

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Handlers;
+
+use Intervention\Image\Facades\Image;
+
+class ImageUploadHandler
+{
+    protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
+
+    public function save($file, $folder, $file_prefix, $max_width = false)
+    {
+        $folder_name = 'uploads/images/' . $folder . '/' . date('Ym/d', time());
+        $upload_path = public_path() . '/' . $folder_name;
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
+
+        if (!in_array($extension, $this->allowed_ext)) {
+            return false;
+        }
+
+        $file->move($upload_path, $filename);
+
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
+        return [
+            'path' => '/' . $folder_name . '/' . $filename,
+        ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        $image->save();
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -18,9 +19,18 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(User $user, UserRequest $request)
+    public function update(User $user, UserRequest $request, ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        // dd($request->avatar);
+        if ($request->file('avatar')) {
+            $result = $uploader->save($request->file('avatar'), 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        // dd($request->file('avatar'));
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
