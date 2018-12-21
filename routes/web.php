e592af58ef17861e92ef2e5ebd5d032126f40a33
921t <Timi921t@gmail.com>
2018-12-21 07:06:23 +0000

    生成话题模块代码

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'PagesController@root')->name('pages.root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
