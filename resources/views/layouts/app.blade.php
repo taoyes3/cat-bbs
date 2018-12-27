9e0c52b24eb2df69148b2767b8d835630c6c27f8
921t <Timi921t@gmail.com>
2018-12-27 08:41:55 +0000

    话题显示页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'Cat BBS')</title>
+        <meta name="description" content="@yield('description', 'Cat BBS')">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @yield('styles')
     </head>
