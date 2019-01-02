9f239090c9d8aab392e4c844cfa0c61e3975a85d
921t <Timi921t@gmail.com>
2019-01-02 03:32:12 +0000

    话题seo

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-
+use App\Handlers\SlugTranslateHandler;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -12,6 +12,11 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
+
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
