5424067a2c824000ef4a3cf07e818cef66e72fbf
921t <Timi921t@gmail.com>
2019-01-15 12:11:39 +0000

    Revert "后台角色权限配置"
    This reverts commit 8a5765a0fc63ea61b5e902490ca7882a3764aae8.

--- database/migrations/2019_01_04_205100_add_notification_count_to_users_table.php
+++ database/migrations/2019_01_04_205100_add_notification_count_to_users_table.php
@@ -26,7 +26,7 @@ class AddNotificationCountToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('notification_count');
+            $table->dropColumn('notification');
         });
     }
 }
