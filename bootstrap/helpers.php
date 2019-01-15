04581de82058cdc390456c78576d2a8ee788934c
921t <Timi921t@gmail.com>
2019-01-15 12:10:11 +0000

    Revert "后台用户curd"
    This reverts commit 8f692ca27e61c7250fc8be8295fc504b9a9db68a.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -10,24 +10,3 @@ function make_excerpt($value, $length = 200)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/', '', strip_tags($value)));
     return str_limit($excerpt, $length);
 }
-
-if (!function_exists('manage_contents')) {
-    function manage_contents()
-    {
-        return Auth::check() && Auth::user()->can('manage_contents');
-    }
-}
-
-if (!function_exists('administrator_users_avatar')) {
-    function administrator_users_avatar($avatar, $model)
-    {
-        return empty($avatar) ? 'N/A' : '<img src="' . $avatar . '" width="40">';
-    }
-}
-
-if (!function_exists('administrator_users_name')) {
-    function administrator_users_name($name, $model)
-    {
-        return '<a href="/users/' . $model->id . '" target=_blank>' . $name . '</a>';
-    }
-}
